0af2c4ab650eaa2d5145f60fdc6cce1d43839f04
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-08 14:15:41 -0500

    finished Christmas Birthdays

--- where_exercises.sql
+++ where_exercises.sql
@@ -13,3 +13,7 @@ where last_name like 'e%';
 select first_name, last_name, hire_date
 from employees
 where hire_date between '1990-01-01' and '1999-12-31';
+
+select * 
+from employees
+where birth_date like '%12-25';

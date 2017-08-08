cda2ca8cf0eb7ac3f5b0e2d944823fc97759741f
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-08 14:12:06 -0500

    Finished 'employees in the 90s' /sick synthwave beat/

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,3 +9,7 @@ where first_name in ('Irina', 'Vidya', 'Maya');
 select first_name, last_name
 from employees
 where last_name like 'e%';
+
+select first_name, last_name, hire_date
+from employees
+where hire_date between '1990-01-01' and '1999-12-31';

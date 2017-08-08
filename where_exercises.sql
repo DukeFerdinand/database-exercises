ccd9f04452092fa8fcd756f38255713393a5948f
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-08 14:10:14 -0500

    finished 'where like'

--- where_exercises.sql
+++ where_exercises.sql
@@ -5,3 +5,7 @@ from employees;
 select first_name, last_name 
 from employees
 where first_name in ('Irina', 'Vidya', 'Maya');
+
+select first_name, last_name
+from employees
+where last_name like 'e%';

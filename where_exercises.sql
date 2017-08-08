3d42765dcabffcfc1954b0d2bd9a8621023b5e8d
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-08 14:44:33 -0500

    Finished While part 2

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,22 +4,31 @@ show tables;
 select *
 from employees;
 
-select first_name, last_name 
+select emp_no, first_name, last_name, gender 
 from employees
-where first_name in ('Irina', 'Vidya', 'Maya');
+/* where first_name in ('Irina', 'Vidya', 'Maya'); */
+where gender = 'M'
+	and ( 
+		first_name = 'Irena'
+		or first_name = 'Vidya'
+		or first_name = 'Maya'
+		);
 
 select first_name, last_name
 from employees
-where last_name like 'e%';
+where last_name like 'e%'
+	and last_name like '%e';
 
 select first_name, last_name, hire_date
 from employees
-where hire_date between '1990-01-01' and '1999-12-31';
+where hire_date between '1990-01-01' and '1999-12-31'
+	and birth_date like '%12-25';
 
-select * 
+/* select * 
 from employees
-where birth_date like '%12-25';
+where  */
 
 select *
 from employees
-where last_name like '%q%';
+where last_name like '%q%'
+and last_name not like '%qu%';

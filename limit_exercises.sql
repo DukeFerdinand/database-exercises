e43d9dc2b89f2216056767ed2e1e4d91b9422ad3
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-09 10:29:02 -0500

    finished last name sort limit

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,22 +1,6 @@
 use employees;
 
-show tables;
-select *
-from employees;
-
-
-select *
-from titles
-group by title ASC;
-
-
-select first_name, last_name
-from employees
-where last_name like'e%e'
-group by first_name, last_name;
-
 select *
 from employees
-where last_name like '%q%'
-and last_name not like '%qu%'
-group by last_name;
+group by last_name desc
+limit 10;

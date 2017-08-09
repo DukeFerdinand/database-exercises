7e5f6734280fe24190150eb0fef123cd95a8b841
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-09 11:43:52 -0500

    Finished first two queries

---
+++ functions_exercises.sql
@@ -0,0 +1,35 @@
+use employees;
+
+show tables;
+select *
+from employees;
+
+select emp_no, first_name, last_name, gender 
+from employees
+/* where first_name in ('Irina', 'Vidya', 'Maya'); */
+where gender = 'M'
+	and ( 
+		first_name = 'Irena'
+		or first_name = 'Vidya'
+		or first_name = 'Maya'
+		)
+ORDER BY first_name ASC, last_name ASC;
+
+select first_name, last_name
+from employees
+where last_name like 'e%'
+	and last_name like '%e';
+
+select first_name, last_name, hire_date
+from employees
+where hire_date between '1990-01-01' and '1999-12-31'
+	and birth_date like '%12-25';
+
+/* select * 
+from employees
+where  */
+
+select *
+from employees
+where last_name like '%q%'
+and last_name not like '%qu%';

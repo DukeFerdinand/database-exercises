25e85b1206014f92d93b7e0cb42d5073f3281172
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-09 09:40:32 -0500

    finished 'e%e' item

--- group_by_exercise.sql
+++ group_by_exercise.sql
@@ -1,5 +1,21 @@
 use employees;
 
+show tables;
+select *
+from employees;
+
+
 select *
 from titles
 group by title ASC;
+
+
+select first_name, last_name
+from employees
+where last_name like'e%e'
+group by first_name, last_name;
+
+select *
+from employees
+where last_name like '%q%'
+and last_name not like '%qu%';

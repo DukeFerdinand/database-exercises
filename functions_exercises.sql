cc95c3b8d04bf87508b91f7e30bd5aa7e8085bb1
Douglass Hooker <DouglassHooker@gmail.com>
2017-08-09 11:46:04 -0500

    Finished Hired in the 90s

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -4,23 +4,19 @@ show tables;
 select *
 from employees;
 
-select emp_no, first_name, last_name, gender 
+select count(*), gender
 from employees
-/* where first_name in ('Irina', 'Vidya', 'Maya'); */
-where gender = 'M'
-	and ( 
-		first_name = 'Irena'
-		or first_name = 'Vidya'
-		or first_name = 'Maya'
-		)
-ORDER BY first_name ASC, last_name ASC;
+where first_name = 'Irina'
+	or first_name = 'Vidya'
+	or first_name = 'Maya'
+group by gender;
 
-select first_name, last_name
+select concat(first_name, ' ', last_name)
 from employees
 where last_name like 'e%'
 	and last_name like '%e';
 
-select first_name, last_name, hire_date
+select datediff(now(), hire_date), first_name
 from employees
 where hire_date between '1990-01-01' and '1999-12-31'
 	and birth_date like '%12-25';
